76dad51aedf734e4c6cd1137c6665845670c5d68
Zuber8960 <zuberahmad8960@gmail.com>
2022-12-18 22:15:02 +0530

    task-22

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -36,6 +38,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, {through: CartItem });
+
 
 const port = 4000;
 sequelize
@@ -52,6 +59,10 @@ sequelize
   })
   .then((user) => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
+    // console.log(cart);
     console.log(`listening to the ${port}`);
     app.listen(port);
   })
